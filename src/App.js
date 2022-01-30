bb3c21f08064ea1bf4118036e5b45c916b3e9240
tunjiakanbi <tunjiakanbi@gmail.com>
2022-01-30 15:22:03 -0600

    animation works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import './App.scss';
+import { Canvas , useFrame } from "react-three-fiber";
+import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei';
+import { useSpring, animated } from '@react-spring/three';
+
+softShadows();
+const SpinningMesh = ({position, args, color, speed})=> {
+// const SpinningMesh = ({position, args, color, wireframe})=> {
+  const mesh = useRef(null);
+  useFrame(()=> mesh.current.rotation.x = mesh.current.rotation.y += 0.01);
+
+  const [expand, setExpand] = useState(false);
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4]: [1, 1, 1]
+  })
+
+  return(
+        <animated.mesh 
+        onClick={()=> setExpand(!expand)}
+        scale={props.scale}
+        castShadow
+        position={position} 
+        ref={mesh}>
+          <boxBufferGeometry attach='geometry' args={args}/>
+          <MeshWobbleMaterial 
+          attach='material' 
+          color={color} 
+          speed={speed} 
+          factor={0.6}
+          />
+          {/* <MeshWobbleMaterial attach='material' color={color} speed={1} factor={0.6}/> */}
+          {/* <meshStandardMaterial attach='material' color={color} wireframe={true}/> */}
+        </animated.mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas 
+      shadows
+      colorManagement 
+      camera={{position:[-5, 2, 10], fov: 60}}>
+        <ambientLight intensity={0.3} />
+        <directionalLight 
+        castShadow
+        position={[0, 10, 0]} 
+        intensity={1} 
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-bias={0.0005}
+        shadow-camera-near={0.01}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.3}/>
+        <pointLight position={[0, -10, 0]} intensity={1.5}/>
+
+        <group>
+          <mesh 
+          receiveShadow
+          rotation={[-Math.PI /2, 0, 0]} 
+          position={[0, -3, 0]}>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            {/* This will need to cast a shadow */}
+            {/* <meshStandardMaterial attach='material' color={'yellow'} /> */}
+            <shadowMaterial attach='material' opacity={0.3}/>
+          </mesh>
+          <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color={'lightblue'} speed={2}/>
+          <SpinningMesh position={[-2, 1, -5]} color={'pink'} speed={6}/>
+          <SpinningMesh position={[5, 1, -2]} color={'pink'} speed={6}/>
+        </group>
+      {/* We move these into the group above */}
+        {/* 
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color={'lightblue'} speed={2}/>
+        <SpinningMesh position={[-2, 1, -5]} color={'pink'} speed={6}/>
+        <SpinningMesh position={[5, 1, -2]} color={'pink'} speed={6}/> */}
+
+        <OrbitControls />
+       
+      </Canvas>
+    </>
   );
 }
 
